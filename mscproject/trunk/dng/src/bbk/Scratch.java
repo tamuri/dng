3a80a9124a425bd0af6d02e93c689968c541a213
tamuri <tamuri@gmail.com>
2008-08-13 00:17:07 +0000

    Added

--- mscproject/trunk/dng/src/bbk/Scratch.java
+++ mscproject/trunk/dng/src/bbk/Scratch.java
@@ -1,13 +1,10 @@
 package bbk;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-
 /**
  * Date: 13-Aug-2008 01:03:53
  */
 public class Scratch {
     public static void main(String[] args) {
-        Analyzer a = new StandardAnalyzer();
+        System.out.println("Hello, world!");
     }
 }
