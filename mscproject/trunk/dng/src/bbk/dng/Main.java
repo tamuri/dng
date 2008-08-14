9d11ba1508f5dd363171097b8383bdb63236ca29
tamuri <tamuri@gmail.com>
2008-08-14 10:06:48 +0000

    Extracted button1 action performed into its own method

--- mscproject/trunk/dng/src/bbk/dng/Main.java
+++ mscproject/trunk/dng/src/bbk/dng/Main.java
@@ -56,49 +56,7 @@ public class Main {
         button1.setText("Search");
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                System.out.printf("You typed '%s'\n", textField1.getText());
-                ArrayList<String> architectures = null;
-                try {
-                    // find domains for this sequence
-                    ArrayList<String> domains = searcher.getDomainsBySequence(textField1.getText());
-                    for (String d: domains) {
-                        System.out.printf("%s\n", d);
-                    }
-
-                    // find architectures with these domains
-                    architectures = searcher.getArchitecturesByDomains(domains);
-                    for (String a:architectures) {
-                        System.out.printf("%s\n", a);
-                    }
-
-                } catch (Exception ex) {
-                    System.out.printf("Error searching with SwissPfamSearcher.\n");
-                }
-
-                Random r = new Random();
-                int count = 1;
-
-                if (architectures != null) {
-                    System.out.printf("here");
-                    Graph g = new Graph();
-
-                    for (String a: architectures) {
-                        Node n1 = g.addNode();
-                        int i = r.nextInt(count);
-                        g.addEdge(n1, g.getNode(i));
-                        count++;
-                    }
-
-    //                g.addEdge(3, 6);
-    //                g.addEdge(6, 0);
-                    graphPanel.getVisualization().removeGroup("graph");
-                    graphPanel.getVisualization().addGraph("graph", g);
-                    graphPanel.getVisualization().setValue("graph.nodes", null, VisualItem.SHAPE, Constants.SHAPE_ELLIPSE);
-                }
-
-               // graphPanel.getVisualization().repaint();
-                graphPanel.getVisualization().run("layout");
-
+                button1ActionPerformed(e);
             }
         });
         inputPanel.add(button1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
@@ -112,6 +70,49 @@ public class Main {
         // Pack and display window
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private void button1ActionPerformed(ActionEvent e) {
+        System.out.printf("You typed '%s'\n", textField1.getText());
+        ArrayList<String> architectures = null;
+        try {
+            // find domains for this sequence
+            ArrayList<String> domains = searcher.getDomainsBySequence(textField1.getText());
+            for (String d: domains) {
+                System.out.printf("%s\n", d);
+            }
+
+            // find architectures with these domains
+            architectures = searcher.getArchitecturesByDomains(domains);
+            for (String a:architectures) {
+                System.out.printf("%s\n", a);
+            }
+
+        } catch (Exception ex) {
+            System.out.printf("Error searching with SwissPfamSearcher.\n");
+        }
+
+        Random r = new Random();
+        int count = 1;
+
+        if (architectures != null) {
+            System.out.printf("here");
+            Graph g = new Graph();
+
+            for (String a: architectures) {
+                Node n1 = g.addNode();
+                int i = r.nextInt(count);
+                g.addEdge(n1, g.getNode(i));
+                count++;
+            }
+
+            graphPanel.getVisualization().removeGroup("graph");
+            graphPanel.getVisualization().addGraph("graph", g);
+            graphPanel.getVisualization().setValue("graph.nodes", null, VisualItem.SHAPE, Constants.SHAPE_ELLIPSE);
+        }
+
+        // graphPanel.getVisualization().repaint();
+        graphPanel.getVisualization().run("layout");
     }
 
     public static void main(String[] args) {
