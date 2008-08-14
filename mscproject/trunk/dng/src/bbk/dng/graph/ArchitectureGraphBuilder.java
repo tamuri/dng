2078c3dfcb96746465a58f01a2ad530abef07d26
tamuri <tamuri@gmail.com>
2008-08-14 22:16:48 +0000

    Reverting back to the original, simple, addEdgesByMatrix - based on parent architecture, connected/unconnected lists.

--- mscproject/trunk/dng/src/bbk/dng/graph/ArchitectureGraphBuilder.java
+++ mscproject/trunk/dng/src/bbk/dng/graph/ArchitectureGraphBuilder.java
@@ -31,7 +31,7 @@ public class ArchitectureGraphBuilder {
         return g;
     }
 
-    public Graph addEdgesByMatrix2(Graph g, Map<Pair<String,String>, Double> matrix, String parentArchitecture) {
+    public Graph addEdgesByMatrix(Graph g, Map<Pair<String,String>, Double> matrix, String parentArchitecture) {
         Table graphTable = g.getNodeTable();
 
         Set<String> connected = new HashSet<String>();
@@ -109,90 +109,6 @@ public class ArchitectureGraphBuilder {
             System.out.printf("Connected:%s + %s = %s (=%s)",connected.size(), unconnected.size(), connected.size() + unconnected.size(),
                     total);
             System.out.printf("Total edges:%s\n", edgeTable.getRowCount());
-        }
-
-        return g;
-    }
-
-    public Graph addEdgesByMatrix(Graph g, Map<Pair<String,String>, Double> matrix, String parentArchitecture) {
-        Table graphTable = g.getNodeTable();
-
-        Set<String> connected = new HashSet<String>();
-        Set<String> unconnected = new HashSet<String>();
-        Map<String, Integer> architectureNodeId = new HashMap<String, Integer>();
-
-
-        connected.add(parentArchitecture);
-
-        int total = 0;
-        for (int i = 0; i < graphTable.getRowCount(); i++) {
-            String architecture = graphTable.getString(i, "name");
-            architectureNodeId.put(architecture, i);
-            if (!architecture.equals(parentArchitecture)) {
-                unconnected.add(architecture);
-            }
-            total++;
-        }
-
-        Table edgeTable = g.getEdgeTable();
-
-        while (unconnected.size() > 0) {
-
-            double maxscore = -999999;
-            ArrayList<String> targetsToRemove = new ArrayList<String>();
-            ArrayList<Pair<Integer,Integer>> toConnect = new ArrayList<Pair<Integer,Integer>>();
-
-            for (Pair<String,String> arch: matrix.keySet()) {
-                double thisScore = matrix.get(arch);
-                if (!Tuple.get1(arch).equals(Tuple.get2(arch)) && unconnected.contains(Tuple.get2(arch))) {
-                    // if we haven't already added its opposite
-                    if (!toConnect.contains(Tuple.from(architectureNodeId.get(Tuple.get2(arch)), architectureNodeId.get(Tuple.get1(arch))))) {
-                        if (thisScore > maxscore) {
-                            targetsToRemove.clear();
-                            targetsToRemove.add(Tuple.get1(arch));
-                            targetsToRemove.add(Tuple.get2(arch));
-                            toConnect.clear();
-                            toConnect.add(Tuple.from(architectureNodeId.get(Tuple.get1(arch)), architectureNodeId.get(Tuple.get2(arch))));
-                            maxscore = thisScore;
-                        } else if (thisScore == maxscore) {
-                            targetsToRemove.add(Tuple.get1(arch));
-                            targetsToRemove.add(Tuple.get2(arch));
-                            toConnect.add(Tuple.from(architectureNodeId.get(Tuple.get1(arch)), architectureNodeId.get(Tuple.get2(arch))));
-                        }
-                    }
-                }
-            }
-
-            if (targetsToRemove.size() > 0 && toConnect.size() > 0) {
-                for(Pair<Integer,Integer> nodePair: toConnect) {
-                    int row = edgeTable.addRow();
-                    System.out.printf("joining with score (%s) %s -> %s\n", maxscore, Tuple.get1(nodePair), Tuple.get2(nodePair));
-                    edgeTable.setInt(row, "source", Tuple.get1(nodePair));
-                    edgeTable.setInt(row, "target", Tuple.get2(nodePair));
-                    edgeTable.setString(row, "name", Double.toString(maxscore));
-                }
-
-
-                for(String target: targetsToRemove) {
-                    ArrayList<String> tmp = new ArrayList<String>();
-                    for (String u: unconnected) {
-                        if (u.equals(target)) {
-                            tmp.add(u);
-                        }
-                    }
-
-                    for (String t: tmp) {
-                        boolean b = unconnected.remove(t);
-                        System.out.printf("removed %s - %s\n", b, t);
-                    }
-                    connected.add(target);
-                }
-
-            }
-            System.out.printf("Connected:%s + %s = %s (=%s)",connected.size(), unconnected.size(), connected.size() + unconnected.size(),
-                    total);
-            System.out.printf("Total edges:%s\n", edgeTable.getRowCount());
-
         }
 
         return g;
